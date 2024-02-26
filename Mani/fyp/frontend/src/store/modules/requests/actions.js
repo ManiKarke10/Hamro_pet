9bc4df9267b40f613fcee2ef3b08426f7fe92da1
Mani karki <np03cs4m210016@heraldcollege.edu.np>
2024-02-26 22:23:18 +0545

    changes

--- Mani/fyp/frontend/src/store/modules/requests/actions.js
+++ Mani/fyp/frontend/src/store/modules/requests/actions.js
@@ -43,8 +43,7 @@ export default {
 
     // Handle payment response as needed
     console.log('Payment Response:', paymentResponse.data.data.payment_url);
-    window.open(paymentResponse.data.data.payment_url);
-    window.close();
+    window.location.href= paymentResponse.data.data.payment_url;
     } 
     
     catch (error) {
